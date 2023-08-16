a3e76fbeeb3475961ab6d7f18e928c3a7806e829
Sofia Cipollone <sofia.cipollone5@gmail.com>
2023-08-17 00:33:04 +0200

    Added command line parser, added an option to specify a directory

--- pdfpc.py
+++ pdfpc.py
@@ -1,46 +1,52 @@
 import sys
 import os
+import argparse
 import PyPDF2
 from tabulate import tabulate
 
-def main():
-    num_args = len(sys.argv)
-    if num_args > 2:
-        print("Cannot invoke with that amount of args.", file=sys.stderr)
-        quit()
-
-    currentDir = os.getcwd()
+def main(args):
     fileTable = []
     totalPages = 0
-    for file in os.listdir(currentDir):
-        if not file.endswith('.pdf'):
+    pdfFileCounter = 0
+    for fileName in os.listdir(args.dir):
+        if not fileName.endswith('.pdf'):
             continue
-        pages = count_pages(file)
+        # Reconstructing the full file path.
+        pages = count_pages(f"{args.dir}{os.path.sep}{fileName}")
         totalPages += pages
-        fileTable.append([file, pages])
+        fileTable.append([fileName, pages])
+        pdfFileCounter += 1
+
+    if pdfFileCounter == 0:
+        print("pdfpc: error: No PDF file(s) found in this directory.", file=sys.stderr)
+        quit()
 
     fileTable.append([])
     fileTable.append(['Total', totalPages])
 
-    if num_args == 2:
-        days = int(sys.argv[1])
-        if days <= 0:
-            print("Cannot invoke with a 0 or negative amount of days.", file=sys.stderr)
-            quit()
-        pagesPerDay = totalPages / days
+    if args.days is not None:
+        if args.days <= 0:
+            parser.error("Cannot invoke with a 0 or negative amount of days.")
+        pagesPerDay = totalPages / args.days
         fileTable.append(['Pages/Day', round(pagesPerDay)])
 
     print(tabulate(fileTable, headers=['File', 'Pages']))
 
-def count_pages(fileName):
+def count_pages(filePath):
     """
     Gets the amount of pages of the given pdf file.
     :param fileName: The file name
     :return: The amount of pages
     """
-    file = open(fileName, 'rb')
+    file = open(filePath, 'rb')
     reader = PyPDF2.PdfFileReader(file)
     return reader.numPages
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(prog='pdfpc',
+                                     description='A simple script to count the total amount of pages of multiple PDFs files.')
+    parser.add_argument('-d', '--dir', type=str, nargs='?', default=os.getcwd(),
+                        help='An optional directory path to look for PDFs. Default is cwd.')
+    parser.add_argument('days', metavar='days', type=int, nargs='?',
+                        help='The amount of available days to study the PDFs.')
+    main(parser.parse_args())
