648a7f0a1a5257a0c33066d55eeedb1b813a40fd
Sofia Cipollone <sofia.cipollone5@gmail.com>
2023-08-18 22:30:33 +0200

    Added parsing to select a file range, refactored arg days to be an option

--- pdfpc.py
+++ pdfpc.py
@@ -1,25 +1,29 @@
-import sys
 import os
 import argparse
 import PyPDF2
 from tabulate import tabulate
 
 def main(args):
+    if args.range is not None and args.range[0] < 0 or args.range[0] >= args.range[1]:
+        parser.error("Invalid range.")
     fileTable = []
     totalPages = 0
     pdfFileCounter = 0
+    canAdd = True if args.range is None else False
     for fileName in os.listdir(args.dir):
         if not fileName.endswith('.pdf'):
             continue
-        # Reconstructing the full file path.
-        pages = count_pages(f"{args.dir}{os.path.sep}{fileName}")
-        totalPages += pages
-        fileTable.append([fileName, pages])
         pdfFileCounter += 1
+        if pdfFileCounter > args.range[1]:
+            break
+        if canAdd or pdfFileCounter >= args.range[0] and pdfFileCounter <= args.range[1]:
+            # Reconstructing the full file path.
+            pages = count_pages(f"{args.dir}{os.path.sep}{fileName}")
+            totalPages += pages
+            fileTable.append([fileName, pages])
 
     if pdfFileCounter == 0:
-        print("pdfpc: error: No PDF file(s) found in this directory.", file=sys.stderr)
-        quit()
+        parser.error("pdfpc: error: No PDF file(s) found in this directory.")
 
     fileTable.append([])
     fileTable.append(['Total', totalPages])
@@ -47,6 +51,8 @@ if __name__ == "__main__":
                                      description='A simple script to count the total amount of pages of multiple PDFs files.')
     parser.add_argument('-d', '--dir', type=str, nargs='?', default=os.getcwd(),
                         help='An optional directory path to look for PDFs. Default is cwd.')
-    parser.add_argument('days', metavar='days', type=int, nargs='?',
+    parser.add_argument('-r', '--range', metavar=('start', 'end'), type=int, nargs=2,
+                        help='An optional inclusive range to use when considering where to start scanning the files.')
+    parser.add_argument('-D', '--days', metavar='days', type=int, nargs='?',
                         help='The amount of available days to study the PDFs.')
     main(parser.parse_args())
